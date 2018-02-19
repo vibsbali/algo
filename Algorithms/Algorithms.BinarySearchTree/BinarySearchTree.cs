aacfc25020c4f8152f615956559bc6da91421ad7
Bali Vaibhav <bail.vaibhav@recreo.com.au>
2018-02-20 09:55:34 +1100

    - New project for Binary Search Tree

---
+++ Algorithms/Algorithms.BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+[assembly: InternalsVisibleTo("Algorithms.BinarySearchTreesTests")]
+
+namespace Algorithms.BinarySearchTrees
+{
+    public class BinarySearchTree<T> : IEnumerable<T>
+        where T : IComparable
+    {
+        protected internal class BinaryNode : IComparable<BinaryNode>
+        {
+            public BinaryNode(T value)
+            {
+                Value = value;
+            }
+            public BinaryNode LeftChild { get; set; }
+            public BinaryNode RightChild { get; set; }
+
+            public T Value { get; set; }
+
+            public int CompareTo(BinaryNode other)
+            {
+                return this.Value.CompareTo(other.Value);
+            }
+        }
+
+        internal BinaryNode Head { get; private set; }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(T item)
+        {
+            var node = new BinaryNode(item);
+            if (Head == null)
+            {
+                Head = node;
+            }
+            else
+            {
+                var currentNode = Head;
+                AddRecursively(currentNode, node);
+            }
+
+            ++Count;
+        }
+
+        private void AddRecursively(BinaryNode currentNode, BinaryNode newNode)
+        {
+            if (newNode.CompareTo(currentNode) >= 0)
+            {
+                if (currentNode.RightChild == null)
+                {
+                    currentNode.RightChild = newNode;
+                    return;
+                }
+
+                AddRecursively(currentNode.RightChild, newNode);
+            }
+            else
+            {
+                if (currentNode.LeftChild == null)
+                {
+                    currentNode.LeftChild = newNode;
+                    return;
+                }
+
+                AddRecursively(currentNode.LeftChild, newNode);
+            }
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            var resultOfFindWithParent = FindWithParent(item);
+            return resultOfFindWithParent.Item1;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Tuple<bool, BinaryNode> FindWithParent(T value)
+        {
+            BinaryNode parentNode = null;
+            BinaryNode currentNode = Head;
+
+            while (currentNode != null)
+            {
+                if (currentNode.Value.CompareTo(value) == 0)
+                {
+                    return new Tuple<bool, BinaryNode>(true, parentNode);
+                }
+
+                //Update parent node
+                parentNode = currentNode;
+                if (currentNode.Value.CompareTo(value) > 0)
+                {
+                    currentNode = currentNode.LeftChild;
+                }
+                else
+                {
+                    currentNode = currentNode.RightChild;
+                }
+            }
+
+
+            return new Tuple<bool, BinaryNode>(false, parentNode);
+        }
+
+        public int Count { get; private set; }
+        public bool IsReadOnly => false;
+    }
+}
---
+++ Algorithms/Algorithms.BinarySearchTreesTests/BinarySearchTreeTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Algorithms.BinarySearchTrees;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms.BinarySearchTreesTests
+{
+    [TestClass]
+    public class BinarySearchTreeTests
+    {
+        [TestMethod]
+        public void InitialiseNewTree_Assert_Count_Zero()
+        {
+            var binarySearchTree = new BinarySearchTree<int>();
+            Assert.AreEqual(0, binarySearchTree.Count);
+            Assert.IsNotNull(binarySearchTree);
+        }
+
+        [TestMethod]
+        public void InitialiseNewTree_AddOneItem_Assert_Count_One()
+        {
+            var binarySearchTree = new BinarySearchTree<int>();
+            binarySearchTree.Add(100);
+
+            Assert.AreEqual(1, binarySearchTree.Count);
+            Assert.IsNotNull(binarySearchTree);
+            Assert.AreEqual(100, binarySearchTree.Head.Value);
+        }
+
+
+        [TestMethod]
+        public void InitialiseNewTree_AddThreeItems_Of_DifferentValue__Assert_Count_Three_And_Check_Left_Right()
+        {
+            var binarySearchTree = new BinarySearchTree<int>();
+            binarySearchTree.Add(100);
+            binarySearchTree.Add(75);
+            binarySearchTree.Add(125);
+
+            Assert.AreEqual(3, binarySearchTree.Count);
+            Assert.IsNotNull(binarySearchTree);
+            Assert.AreEqual(100, binarySearchTree.Head.Value);
+            Assert.AreEqual(75, binarySearchTree.Head.LeftChild.Value);
+            Assert.AreEqual(125, binarySearchTree.Head.RightChild.Value);
+        }
+
+        [TestMethod]
+        public void InitialiseNewTree_AddThreeItems_Of_SameValue_Assert_Count_Three_And_Check_Left_Right()
+        {
+            var binarySearchTree = new BinarySearchTree<int>();
+            binarySearchTree.Add(100);
+            binarySearchTree.Add(75);
+            binarySearchTree.Add(100);
+
+            Assert.AreEqual(3, binarySearchTree.Count);
+            Assert.IsNotNull(binarySearchTree);
+            Assert.AreEqual(100, binarySearchTree.Head.Value);
+            Assert.AreEqual(75, binarySearchTree.Head.LeftChild.Value);
+            Assert.AreEqual(100, binarySearchTree.Head.RightChild.Value);
+        }
+
+    }
+}
